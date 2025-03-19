7b2beaad637621e0ad5b7268d0db48e09a7e6d10
labhi_git_user <labhi.git@labhi.in>
2025-03-19 14:16:37 +0530

    code clean up

--- backend/dist/common/bcrypt.service.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class bcryptService {
-    private static readonly SALT_ROUNDS;
-    static hashData(data: string): Promise<string>;
-    static compareData(data: string, hashedData: string): Promise<boolean>;
-}
--- backend/dist/modules/auth/forgetPasswordAuth/dto/forgetPassword.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class ForgetPasswordUserDto {
-    email?: string;
-    mobile?: string;
-    emailOrMobile: string;
-    otp?: string;
-    password?: string;
-}
