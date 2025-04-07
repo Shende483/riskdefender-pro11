2b99fcc4bdf9a11b05b364b0f15bf466de9c85fb
suchitaK <amisha.satpaise@labhi.in>
2025-04-07 22:19:47 +0530

    create api for user trading existing data

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,6 +14,7 @@ import { BrokerAccountModule } from './modules/BrokerAccountManagement/brokerAcc
 import { RedisService } from './config/redis.config';
 import { Module } from '@nestjs/common';
 import { UserAccountDetailModule } from './modules/adminModules/UserAccountDetail/userAccountDetail.module';
+import { UserTradingExistingModule } from './modules/adminModules/UserTradingExistingData/userTradingExistingData.module';
 
 @Module({
   imports: [
@@ -31,6 +32,7 @@ import { UserAccountDetailModule } from './modules/adminModules/UserAccountDetai
     TradingRulesModule,
     BrokerAccountModule,
     UserAccountDetailModule,
+    UserTradingExistingModule,
   ],
   providers: [RedisService],
   exports: [RedisService],
